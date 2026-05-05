e4e7417909a529cb6a0c6f28dfa882b300b0894f
aa93221-cpu <aa93221@gmail.com>
2026-05-05 22:25:06 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,35 @@
+const CACHE_NAME = 'pwa-v1';
+
+const FILES_TO_CACHE = [
+  '/123/',
+  '/123/index.html',
+  '/123/manifest.json',
+  '/123/icon.png'
+];
+
 self.addEventListener('install', event => {
-  console.log('Service Worker Installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
 });
 
 self.addEventListener('fetch', event => {
-  // 最簡版，不做快取
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
